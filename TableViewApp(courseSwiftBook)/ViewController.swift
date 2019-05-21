43fee3f21988e4acfb518bd879a73fda92e0ab00
Anton Antonov <a.antonovich.xxvi@gmail.com>
2019-05-21 11:22:03 +0500

    Initial Commit

---
+++ TableViewApp(courseSwiftBook)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewApp(courseSwiftBook)
+//
+//  Created by Антон Антонов on 21/05/2019.
+//  Copyright © 2019 Антон Антонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
