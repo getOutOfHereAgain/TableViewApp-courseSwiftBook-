866e5b90c38b03b48260c2897c53f7967c5f9778
Anton Antonov <a.antonovich.xxvi@gmail.com>
2019-05-21 16:17:09 +0500

    InitialApp

--- TableViewApp(courseSwiftBook)/ViewController.swift
+++ TableViewApp(courseSwiftBook)/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let namedOfPlaces = ["Балкан Гриль", "Бочка", "Вкусные истории", "Дастархан", "Индокитай", "Классик", "Шок", "Bonsai", "Burger Heroes", "Kitchen", "Love&Life", "Morris Pub", "Sherlock Holmes", "Speak Easy", "X.O"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return namedOfPlaces.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = namedOfPlaces[indexPath.row]
+        cell.imageView?.image = UIImage(named: namedOfPlaces[indexPath.row])
+        return cell
+    }
+    
 
 }
 
